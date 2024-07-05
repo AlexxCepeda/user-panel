37ebbc37f7e689f94019666b1ec9b820157e70ee
Alexis Cepeda <alexis9932@gmail.com>
2024-07-05 17:50:13 -0600

    General cleaning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container mx-auto px-4">
+    <div class="mt-32">Home view</div>
   </main>
 </template>
