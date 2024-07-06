b1597e9bfff26ee7d58dc7b73db9e25572789500
Alexis Cepeda <alexis9932@gmail.com>
2024-07-06 17:04:09 -0600

    Component for country filter created

---
+++ src/components/Table/CountryFilter.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="flex flex-col items-start">
+    <label class="sr-only">Country</label>
+    <div class="relative w-full">
+      <input
+        type="text"
+        placeholder="Country"
+        :value="modelValue"
+        @input="updateValue($event.target.value)"
+        class="bg-gray-50 border border-gray-300 text-gray-900 p-2 rounded-md"
+      />
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  modelValue: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const updateValue = (value) => {
+  emit("update:modelValue", value);
+};
+</script>
