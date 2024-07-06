da0b9e2b9497b8f6afda96f3c3a8fdd094064401
Alexis Cepeda <alexis9932@gmail.com>
2024-07-06 17:05:06 -0600

    Component for filter by gender

---
+++ src/components/Table/GenderFilter.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="flex flex-row gap-2">
+    <label
+      v-for="option in options"
+      :key="option.value"
+      class="flex items-center"
+    >
+      <input
+        type="radio"
+        :value="option.value"
+        :checked="modelValue === option.value"
+        @change="updateValue(option.value)"
+        class="mr-2"
+      />
+      <span class="capitalize">{{ option.label }}</span>
+    </label>
+  </form>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: Array,
+    required: true,
+    validator(value) {
+      return value.every((option) => "value" in option && "label" in option);
+    },
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const updateValue = (value) => {
+  emit("update:modelValue", value);
+};
+</script>
