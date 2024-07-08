a7866cf44e3d43ad82de9627f72dfc0a45df2919
Alexis Cepeda <alexis9932@gmail.com>
2024-07-08 09:55:19 -0600

    Router-link added to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="flex items-center justify-center h-screen">
+  <div class="flex items-center flex-col justify-center vertical-center">
     <h1 class="text-center text-2xl text-sky-400 font-bold">
       Welcome to your users panel!
     </h1>
+    <RouterLink :to="{ name: 'users' }" class="underline">Go users</RouterLink>
   </div>
 </template>
 
