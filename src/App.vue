e35fe52ae99721d98dba3820885ffb5e0a5e27cc
Alexis Cepeda <alexis9932@gmail.com>
2024-07-08 11:39:24 -0600

    Footer component added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <main class="vertical-center container mx-auto px-10 py-5">
     <RouterView />
   </main>
+  <Footer />
   <LoadingModal />
 </template>
 
@@ -10,4 +11,5 @@
 import { RouterView } from "vue-router";
 import HeaderNav from "./components/Navbar/HeaderNav.vue";
 import LoadingModal from "./components/LoadingModal.vue";
+import Footer from "./components/Footer.vue";
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-6 bg-gray-700 flex items-center justify-center gap-3">
+    <p class="text-white">Made with Vue.js 3, Pinia, Axios and Tailwind</p>
+    <a
+      href="https://github.com/AlexxCepeda/user-panel"
+      class="underline text-white"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Github</a
+    >
+  </div>
+</template>
